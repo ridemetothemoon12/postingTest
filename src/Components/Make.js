a748b87fa0e8ef195d51fafccfea4fdbfcb72f5b
ridemetothemoon12 <mhhuh12@naver.com>
2022-11-17 21:56:56 +0900

    update2

--- src/Components/Make.js
+++ src/Components/Make.js
@@ -6,7 +6,9 @@ function Make() {
     const [nameInput, setNameInput] = useState('');
     const [idInput, setIdInput] = useState('');
     const [passwordInput, setPasswordInput] = useState('');
+    const [confirmPasswordInput, setConfirmPasswordInput] = useState('');
 
+    const [passwordMessage, setPasswordMessage] = useState('');
     const handleUserName = (e) => {
         setNameInput(e.target.value)
         input.current[0].focus();
@@ -19,8 +21,22 @@ function Make() {
         setPasswordInput(e.target.value)
         input.current[2].focus();
     }
-    
+    const handleUserConfirmPassword = (e) => {
+        setConfirmPasswordInput(e.target.value)
+        input.current[3].focus();
 
+        if(e.target.value.length !== 0) {
+            if(passwordInput === e.target.value) {
+                setPasswordMessage("맞다")
+            }
+            else {
+                setPasswordMessage("틀렸다")
+            }
+        }
+        else {
+            setPasswordMessage("")
+        }
+    }
 
     return (
         <>
@@ -38,11 +54,12 @@ function Make() {
                         </div>
                         <div className='w-[90%] h-24 mx-auto my-10 relative'>
                             <p className={`${((passwordInput.length !== 0) ? 'top-[-10%] text-orange-500 text-xs' : 'top-3')} text-slate-500 absolute duration-200 left-1`}>비밀번호</p>
-                            <input name='password' ref={(e) => (input.current[2] = e)} onChange={handleUserPassword} className='w-full h-[50%] focus:outline-none border-b-2 border-black'></input>
+                            <input type={'password'} name='password' ref={(e) => (input.current[2] = e)} onChange={handleUserPassword} className='w-full h-[50%] focus:outline-none border-b-2 border-black'></input>
                         </div>
                         <div className='w-[90%] h-24 mx-auto my-10 relative'>
-                            <p className='text-slate-500 absolute top-3 left-1'>비밀번호 확인</p>
-                            <input className='w-full h-[50%] bg-slate-300'></input>
+                            <p className={`${((confirmPasswordInput.length !== 0) ? 'top-[-10%] text-orange-500 text-xs' : 'top-3')} text-slate-500 absolute duration-200 left-1`}>비밀번호 확인</p>
+                            <input type={'password'} name='password' ref={(e) => (input.current[3] = e)} onChange={handleUserConfirmPassword} className='w-full h-[50%] focus:outline-none border-b-2 border-black'></input>
+                            <p className='text-slate-500 text-sm'>{passwordMessage}</p>
                         </div>
                     </form>
                     <button type="submit" className='w-11/12 h-12 bg-blue-300 mx-auto rounded-lg flex justify-center items-center'>
